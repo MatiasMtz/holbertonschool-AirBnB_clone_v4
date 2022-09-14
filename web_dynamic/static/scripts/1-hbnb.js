19a46dd6cdcf565ccff12d0b9492fe36c92ceb9b
MaxiHBTN <4387@holbertonschool.com>
2022-09-14 16:08:54 +0000

    Task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,10 @@
+$( document ).ready(function(){
+    let amenity_dic = {};
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            amenity_dic['amenity_id'] = $(this).attr('data_id'); 
+        } else {
+          console.log("Checkbox is not checked..");
+        }
+        )
+});
