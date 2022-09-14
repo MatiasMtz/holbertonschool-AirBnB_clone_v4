6ecf558aec0b1e73efa6633a1ac9f64f6025ba56
MatiasMtz <4442@holbertonschool.com>
2022-09-14 16:11:26 +0000

    fixed script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,9 @@
-$( document ).ready(function(){
+$(document).ready(function(){
     let amenity_dic = {};
     checkbox.addEventListener('change', function() {
         if (this.checked) {
             amenity_dic['amenity_id'] = $(this).attr('data_id'); 
         } else {
           console.log("Checkbox is not checked..");
-        }
-        )
+        }});
 });
